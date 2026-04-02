4817f77bef1d1d9fa334b3c589bd05d5110a6ae4
Shubham Yadav <shubhamlaxmanyadav2@gmail.com>
2026-04-02 12:26:00 +0530

    Aditya Commit

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello I am Suraj Yadav from CMPN div:C Roll no. : 60"
+    return "Hello I am Aditya Yadav from CMPN div:C Roll no. : 48"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
