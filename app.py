eb011d777a3eb112f6bc1d34f3fcba6e006bf4a7
Shubham Yadav <shubhamlaxmanyadav2@gmail.com>
2026-04-02 11:46:35 +0530

    Lab Commit

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello! DevOps Practical Running Successfully"
+    return "Hello I am Shubham Yadav from CMPN div:C Roll no. : 58"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
